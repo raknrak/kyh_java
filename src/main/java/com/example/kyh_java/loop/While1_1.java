b0f38fc1d5a696c8c5342762358311f9bf114690
Gyeongrak Choi <peter6617@gmail.com>
2023-12-17 13:46:28 +0900

    1을 한 번씩 더해서 3번 더하기

---
+++ src/main/java/com/example/kyh_java/loop/While1_1.java
@@ -0,0 +1,18 @@
+package com.example.kyh_java.loop;
+
+public class While1_1 {
+
+    //1을 한 번씩 더해서 총 3번 더하는 코드
+    public static void main(String[] args) {
+        int count = 0;
+
+        count = count+1;
+        System.out.println("현재 숫자는 : " + count ); //현재 숫자는 : 1
+        count = count+1;
+        System.out.println("현재 숫자는 : " + count ); //현재 숫자는 : 2
+        count = count+1;
+        System.out.println("현재 숫자는 : " + count ); //현재 숫자는 : 3
+
+
+    }
+}
