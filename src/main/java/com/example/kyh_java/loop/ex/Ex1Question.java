0dd883e3efeee4acfd59914625869725a46484a4
Gyeongrak Choi <peter6617@gmail.com>
2023-12-17 15:54:12 +0900

    while, for 두 가지 버전으로 숫자 출력하기

--- src/main/java/com/example/kyh_java/loop/ex/Ex1Question.java
+++ src/main/java/com/example/kyh_java/loop/ex/Ex1Question.java
@@ -5,13 +5,13 @@ package com.example.kyh_java.loop.ex;
 while와 for문 두 가지 버전으로 작성하기.*/
 public class Ex1Question {
     public static void main(String[] args) {
-        System.out.println("while 버전");
+        System.out.println("--while 버전--");
         int count = 1;
         while(count <=10){
             System.out.println(count);
             count++;
         }
-        System.out.println("for 버전");
+        System.out.println("--for 버전--");
         for(count = 1; count <=10; count ++){
             System.out.println(count);
         }
