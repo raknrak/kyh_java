b6938d62f9e403d690f6a88880fccacf282fb5ae
Gyeongrak Choi <peter6617@gmail.com>
2023-12-17 13:50:00 +0900

    while을 사용하여 더하기

---
+++ src/main/java/com/example/kyh_java/loop/While1_2.java
@@ -0,0 +1,17 @@
+package com.example.kyh_java.loop;
+
+public class While1_2 {
+    public static void main(String[] args) {
+        int count = 0;
+
+        while(count <3){
+            //count = count + 1;
+            count++;
+            System.out.println("현재 숫자는 : " + count );
+            /*현재 숫자는 : 1
+              현재 숫자는 : 2
+              현재 숫자는 : 3*/
+
+        }
+    }
+}
